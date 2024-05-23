3849dfced8172f38bff2e09213db12d0f8016ad8
Sajadizadipanah <138392702+Sajadizadipanah@users.noreply.github.com>
2024-05-24 00:25:03 +0430

    .

--- main.go
+++ main.go
@@ -21,17 +21,13 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-
-
-
-
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
-
-	router.Run("localhost:8080")
+	router.Run(":8080")
+	//router.Run("localhost:8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
